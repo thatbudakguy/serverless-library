9ea51495cccc9185feeea1ea0109496deb29843f
budak <thatbudakguy@gmail.com>
2017-04-06 17:00:59 -0700

    work request

--- alma.js
+++ alma.js
@@ -1,24 +1,12 @@
-const wt = require('webtask-tools');
-const querystring = require('querystring');
-const https = require('https');
 var app = new (require('express'))();
-
-/**
- * Response messages
- */
-
-// const RESPONSE = {
-//    ERROR : {
-//      statusCode : 500,
-//      message: "Something went wrong. Please try again."
-//    },
-//    TIMEOUT : {
-//      statusCode : 504,
-//      message : "Request to the Alma API endpoint timed out."
-//    }
-// };
+var parser = new (require('xml2js')).Parser();
+var wt = require('webtask-tools');
+var querystring = require('querystring');
+var https = require('https');
 
 app.get('/courses', function(req, res) {
+    var output = res;
+    var xml = '';
     var query = querystring.stringify({
         'limit': req.webtaskContext.query.limit || 100,
         'offset': req.webtaskContext.query.offset || 0,
@@ -31,10 +19,33 @@ app.get('/courses', function(req, res) {
         host: req.webtaskContext.data.alma_url,
         path: '/almaws/v1/courses?' + query
     };
-    var alma_req = https.get(options, function(res) {
-        console.log(res);
+    parser.on('error', function(err) {
+        output.writeHead(500, {'Content-Type': 'application/json'});
+        output.end(JSON.stringify({
+            statusCode: 500,
+            message: "XML parser error. ",
+            error: err
+        }));
     });
-    console.log(alma_req);
+    https.get(options, function(res) {
+        res.on('error', function(err) {
+            output.writeHead(500, {'Content-Type': 'application/json'});
+            output.end(JSON.stringify({
+                statusCode: 500,
+                message: "Internal server error.",
+                error: err
+            }));
+        });
+        res.on('data', function(data) {
+            xml += data.toString();
+        });
+        res.on('end', function() {
+            parser.parseString(xml, function(err, json) {
+                output.writeHead(200, {'Content-Type': 'application/json'});
+                output.end(json);
+            });
+        });
+    });
 });
 
 module.exports = wt.fromExpress(app);
